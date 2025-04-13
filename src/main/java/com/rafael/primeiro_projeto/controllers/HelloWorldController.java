25dc641a4ead49fc1fc1cb90b32f05f2dd64840e
Rafael Rocha <rafaelsantoscv_@hotmail.com>
2025-04-13 15:36:35 -0300

    refactor: injetando a classe de serviço automaticamente utilizando a anotação autowired

--- src/main/java/com/rafael/primeiro_projeto/controllers/HelloWorldController.java
+++ src/main/java/com/rafael/primeiro_projeto/controllers/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.rafael.primeiro_projeto.controllers;
 
 import com.rafael.primeiro_projeto.services.HelloWorldService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,11 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/hello-world")
 public class HelloWorldController {
 
+    @Autowired
     private HelloWorldService helloWorldService;
-
-    public HelloWorldController(HelloWorldService helloWorldService) {
-        this.helloWorldService = helloWorldService;
-    }
 
     @GetMapping
     public String helloWorld() {
